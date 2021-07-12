cb0e1a4d7477dd4e99ba15bfb2f610fa9a36c463
Bomi Lee <bibidibovidiboo@gmail.com>
2021-07-12 18:05:25 +0900

    Feat: Convert youtube axios

--- youtube-project/src/service/youtube.js
+++ youtube-project/src/service/youtube.js
@@ -1,32 +1,36 @@
-'use strict';
+import axios from 'axios';
 
 class Youtube {
-  constructor(httpClient) {
-    this.youtube = httpClient;
+  constructor(key) {
+    this.youtube = axios.create({
+      baseURL: 'https://www.googleapis.com/youtube/v3',
+      params: { key: key },
+    });
   }
 
   async mostPopular() {
     const response = await this.youtube.get('videos', {
       params: {
         part: 'snippet',
-        chart: 'mostPopular',
+        chart: 'mostPupular',
         maxResults: 25,
       },
     });
     return response.data.items;
   }
 
-  async search(query) {
+  async search() {
     const response = await this.youtube.get('search', {
       params: {
         part: 'snippet',
         maxResults: 25,
-        type: 'video',
-        q: query,
+        type: 'video'
       },
     });
-    return response.data.items.map(item => ({ ...item, id: item.id.videoId }));
+    return response.data.items;
   }
+
+  
 }
 
 export default Youtube;
